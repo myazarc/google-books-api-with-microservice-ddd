d1f4c07ea020fd76cd9a8210d7e17641ed26508e
myazarc <myazarc@yandex.com>
2022-08-09 01:53:07 +0300

    added configModule setup

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import path from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: path.join(__dirname, '../', '../.env'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
